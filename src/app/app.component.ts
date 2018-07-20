2ecf7382942969f3955b5e942c4e5fe80a01ce2e
River Merrill <rivermerrill.me@gmail.com>
2018-07-20 14:35:54 -0600

    widget layout now saves to localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,50 @@ import { DragulaService } from 'ng2-dragula';
 })
 export class AppComponent implements OnInit {
   title = 'app';
+  public pckry;
 
-  constructor(private dragula: DragulaService) {
-    dragula.setOptions('first-bag', {
-      moves: function(el, container, handle) {
-        return handle.className === 'handle';
-      }
-    });
-   }
+  constructor(private dragula: DragulaService) { }
   ngOnInit() {
-
+    let order = localStorage.getItem('itemsShiftPositions');
+    const grid = document.querySelector('.row');
+    const pckry = new Packery(grid, {
+      itemSelector: '.col-md-3',
+      columnWidth: '.col-md-3',
+      transitionDuration: '0.3s',
+      shiftPercentResize: true,
+      percentPosition: true,
+      initLayout: false
+    });
+    this.pckry = pckry;
+    pckry.getItemElements().forEach(item => {
+      const draggie = new Draggabilly(item);
+      pckry.bindDraggabillyEvents(draggie);
+    });
+    if (order) {
+      order = JSON.parse(order);
+      pckry._resetLayout();
+      pckry.items = order.map(itemPosition => {
+        const itemElem = grid.querySelector('[data-item-id="' + itemPosition.id + '"]');
+        const item = pckry.getItem(itemElem);
+        item.rect.x = parseFloat(itemPosition.x) * pckry.packer.width;
+        return item;
+      });
+      pckry.shiftLayout();
+    } else {
+      pckry.layout();
+    }
+    pckry.on('dragItemPositioned', _ => this.savePositions(pckry));
+  }
+  savePositions(pckry) {
+    const positions = this.getItemsShiftPositions(pckry);
+    localStorage.setItem('itemsShiftPositions', JSON.stringify(positions));
+  }
+  getItemsShiftPositions(pckry) {
+    return pckry.items.map(function (item) {
+      return {
+        id: item.element.getAttribute('data-item-id'),
+        x: item.rect.x / pckry.packer.width
+      };
+    });
   }
 }
