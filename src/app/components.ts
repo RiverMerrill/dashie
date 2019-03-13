3b3cde55db6036e94de31bbcbff4539b0af0f4c4
River Merrill <rivermerrill.me@gmail.com>
2019-03-12 20:37:07 -0600

    blank slate

--- src/app/components.ts
+++
@@ -1,7 +0,0 @@
-import { TodoComponent } from './todo/todo.component';
-import { BitcoinComponent } from './bitcoin/bitcoin.component';
-export class Components {
-    public readonly todo =  { title: 'Todo', description: 'A simple Todo List', component: TodoComponent, selector: 'app-todo'};
-    public readonly bitcoin = { title: 'Bitcoin', description: 'bitcoin tracker', component: BitcoinComponent, selector: 'app-bitcoin'};
-    public list = [this.todo, this.bitcoin];
-}
